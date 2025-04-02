6c88d9db5e518b16979b1144a07ea8bcf8d683a9
Diana Suvorova <diana.suvorova@gmail.com>
2025-04-02 14:10:34 -0700

    chore: cjs publishing

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['esm', 'cjs'],
-  outDir: 'dist',
-  dts: true,
-  splitting: false,
-  clean: true,
-});
+export default defineConfig([
+    {
+      entry: { index: 'src/index.ts' },
+      format: 'esm',
+      outDir: 'dist',
+      dts: false,
+      clean: false,
+      outExtension: () => ({ js: '.js' }),
+    },
+    {
+      entry: { index: 'src/index.ts' },
+      format: 'cjs',
+      outDir: 'dist',
+      dts: true,
+      clean: false,
+      outExtension: () => ({ js: '.cjs' }),
+    },
+  ]);
